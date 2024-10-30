c6dcaa5b774a2b085951e33e284f38920f2045a9
pouchkine <pouchkinecamerdouala@gmail.com>
2024-10-30 12:21:03 +0200

    add comment into methods

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -10,12 +10,18 @@ class BasicCache(BaseCaching):
     return BasicCache
     """
     def put(self, key, item):
+        """
+        put function
+        """
         if key is None:
             pass
         else:
             self.cache_data[key] = item
 
     def get(self, key):
+        """
+        get function
+        """
         if key is None or key not in self.cache_data.keys():
             return None
         else:
