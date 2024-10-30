e520fd1a748f22ee7af317c6df87139ea9feadd4
pouchkine <pouchkinecamerdouala@gmail.com>
2024-10-30 12:08:10 +0200

    fix executable

--- 0-basic_cache.py
+++ 0-basic_cache.py
@@ -10,17 +10,13 @@ class BasicCache(BaseCaching):
     return BasicCache
     """
     def put(self, key, item):
-        if key == None:
+        if key is None:
             pass
         else:
             self.cache_data[key] = item
 
     def get(self, key):
-        if key == None or key not in self.cache_data.keys():
+        if key is None or key not in self.cache_data.keys():
             return None
         else:
             return self.cache_data[key]
-
-
-
-
