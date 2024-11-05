7a5dc4ce42f72520b6f73d844c0cb66361b57b9a
pouchkine <pouchkinecamerdouala@gmail.com>
2024-11-05 14:32:28 +0200

    fix doc

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
+"""
+serving a simple page
+"""
 from flask import Flask, render_template
 
 app = Flask('__main__')
 
 
 @app.route("/")
-def index():
+def index() -> str:
+    """
+    return the index page
+    """
     return render_template('index.html')
 
 
